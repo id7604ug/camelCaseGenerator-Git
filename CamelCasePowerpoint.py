16a69f0940427daf8a0eae00ca2d8c1ee254e6d5
Anthony <@home>
2017-01-19 15:57:25 -0600

    camelCaseGen powerpoint fixed indentation

--- CamelCasePowerpoint.py
+++ CamelCasePowerpoint.py
@@ -4,7 +4,7 @@ def camelcase(word):
     # Slices don't produce index out of bounds errors.
     # So this still works on empty strings, strings of length 1
 def display_banner():
-'''Display program name in a banner'''
+    '''Display program name in a banner'''
     msg = 'AWESOME camelCaseGenerator PROGRAM'
     stars = '*' * len(msg)
     print('\n', stars, '\n', msg, '\n', stars, '\n')
@@ -15,7 +15,7 @@ def main():
     camelcased_words = [ camelcase(word) for word in words ] # camelCase everything
     camelcased_words[0] = camelcased_words[0].lower() # lowercase first word
     output = ''.join(camelcased_words) #Put words back together
-print(output)
+    print(output)
 
 if __name__ == '__main__':
     main()
